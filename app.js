4ab44009947b00542a9e38986f87d60b70db581d
jnbi <creat.bliv@gmail.com>
2021-09-28 00:16:16 +0900

    commit 2

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ import path from "path";
 
 // 2.listen : localhost:PORT의 URL에서 응답
 // 3.USE - Mounts the specified middleware function or functions at the specified path
-
 const port = process.env.PORT || 1234;
 const app = express();
 
-    app.use((req,res,next)=> {console.log(Date.now());
-        
-        res.send("IS IT?");
-        next();});
+const logger1 = (res,req,next) => {console.log("logger 1"); next();};
+const logger2 = (res,req,next) => {console.log("logger 2"); next();};
 
-app.get("/",(req,res)=>{
-    res.send("HELLO world")
+app.get("/",logger1,logger2,(req,res)=>{
+    res.send("NICE MIDDLEWARE");
 })
+
 
 app.listen(port,() => console.log("listenning.."));
